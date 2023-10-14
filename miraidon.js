9a60286abe12c994d61174b5c42dbd000c8beca0
arenashlock <ashlock.aren@gmail.com>
2023-10-14 16:29:46 -0500

    Finished Miraidon page and made minor fixes

--- miraidon.js
+++ miraidon.js
@@ -9,13 +9,14 @@ function loadCards(allCards){
         let cardName = allCards.miraidonCards[i].cardName;
         let set = allCards.miraidonCards[i].set;
         let collectionNumber = allCards.miraidonCards[i].collectionNumber;
+        let purpose = allCards.miraidonCards[i].purpose;
         let cardImage = allCards.miraidonCards[i].cardImage;
 
         let card = document.createElement("div");
         card.innerHTML = `
-        <div class="col">
+        <div class="col" id="${set}-${collectionNumber}">
             <div class="card shadow-sm">
-                <img src="${cardImage}" alt="--------- ADD THIS -----------" class="Pokemon-card-picture">
+                <img src="${cardImage}" alt="${cardName}" class="Pokemon-card-picture">
                 <div class="card-body">
                     <h1>${cardName}</h1>
                     <h2>${set} - ${collectionNumber}</h2>
@@ -24,7 +25,25 @@ function loadCards(allCards){
             </div>
         </div>
         `;
-
         cardsContainer.appendChild(card);
+
+        let cardContainer = document.getElementById("selected-card");
+        let card_chosen = document.getElementById(`${set}-${collectionNumber}`);
+
+        card_chosen.addEventListener("click", function(){
+            cardContainer.innerHTML = `
+            <img src="${cardImage}" alt="${cardName}" id="selected-card-picture">
+            <p>${purpose}</p>
+            `;
+        });
     }
+
+    /*let cardContainer = document.getElementById("selected-card");
+    let card_chosen = document.getElementById("SV01-081");
+
+    card_chosen.addEventListener("click", function(){
+        let cardName = allCards.miraidonCards[0].cardName;
+        let cardImage = allCards.miraidonCards[0].cardImage;
+        cardContainer.innerHTML = `<img style="width: 100%; margin: 10px;" src="${cardImage}" alt="${cardName}" class="Pokemon-card-picture">`;
+    });*/
 }
