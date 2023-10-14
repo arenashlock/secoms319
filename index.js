fb68852d6ee2e6084f0507a290a294fa341588ca
arenashlock <ashlock.aren@gmail.com>
2023-10-14 14:04:27 -0500

    Completed Home page

---
+++ index.js
@@ -0,0 +1,10 @@
+let gardevoir_view = document.getElementById("gardevoir-view");
+let miraidon_view = document.getElementById("miraidon-view");
+
+gardevoir_view.addEventListener("click", function(){
+    location.href = "./gardevoir.html";
+});
+
+miraidon_view.addEventListener("click", function(){
+    location.href = "./miraidon.html";
+});
