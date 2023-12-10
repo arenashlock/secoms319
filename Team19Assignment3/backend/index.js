2e9dda166696b32347c2756d93bc53f845e0e5a8
Aren Ashlock <ashlock.aren@gmail.com>
2023-12-10 12:50:41 -0600

    POST, DELETE functional. Updated images

--- Team19Assignment3/backend/index.js
+++ Team19Assignment3/backend/index.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var cors = require("cors");
+var mongo = require("mongodb");
 var app = express();
 var fs = require("fs");
 var bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
 
 /* ----------------------------------------
         CONNECTING TO SERVER
@@ -15,7 +19,7 @@ const port = "8081";
 const host = "localhost";
 
 app.listen(port, () => {
-    console.log("App listenting at http://%s%s\n", host, port);
+    console.log("App listenting at http://%s:%s\n", host, port);
 });
 
 /* ----------------------------------------
@@ -27,6 +31,37 @@ const url = "mongodb://127.0.0.1:27017";
 const dbName = "reactdata";
 const client = new MongoClient(url);
 const db = client.db(dbName);
+
+/* ----------------------------------------
+        POST NEW REQUEST
+   ---------------------------------------- */
+
+app.post("/add_product", async (req, res) => {
+    await client.connect();
+    console.log("Request: /add_product");
+        
+    const newProduct = req.body; // Data sent in the body of the POST request
+    const query = {
+        title: newProduct.title,
+        price: Number(newProduct.price),
+        description: newProduct.description,
+        category: newProduct.category,
+        image: newProduct.image,
+        rating: {
+            rate: Number(newProduct.rating.rate),
+            count: Number(newProduct.rating.count)
+        }
+    }
+    const result = await db.collection("fakestore_catalog").insertOne(query);
+        
+    if(!result) {
+        res.send("Product was not added").status(404);
+    }
+            
+    else {
+        res.status(201).send(result);
+    }
+});
 
 /* ----------------------------------------
         GET ALL REQUEST
@@ -42,4 +77,25 @@ app.get("/fakestore_catalog", async (req, res) => {
     
     res.status(200);
     res.send(results);
+});
+
+/* ----------------------------------------
+        DELETE ONE REQUEST
+   ---------------------------------------- */
+
+   app.delete("/delete_product", async (req, res) => {
+    await client.connect();
+    console.log("Request: /delete_product");
+    
+    const values = Object.values(req.body);
+    const deleteTitle = values[0]; // Delete title
+
+    console.log(deleteTitle)
+
+    const query = {title: deleteTitle};
+    const result = await db.collection("fakestore_catalog").deleteOne(query);
+    console.log("DELETE Item: ", result);
+    
+    res.status(200);
+    res.send(result);
 });
--- Team19Assignment3/frontend/src/App.js
+++ Team19Assignment3/frontend/src/App.js
@@ -1,10 +1,392 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
+const POST = () => {
+  const [productInformation, setProductInformation] = useState({
+    stateTitle: '',
+    statePrice: null,
+    stateDescription: '',
+    stateCategory: '',
+    stateImage: 'http://localhost:8081/images/',
+    stateRate: null,
+    stateCount: null,
+  });
+
+  function resetStateInformation() {
+    setProductInformation({
+        ...productInformation,
+        stateTitle: '',
+        statePrice: null,
+        stateDescription: '',
+        stateCategory: '',
+        stateImage: 'http://localhost:8081/images/',
+        stateRate: null,
+        stateCount: null,
+    });
+  }
+
+  function handleFieldChangeAdd(field, value) {
+    setProductInformation({
+      ...productInformation,
+      [field]: value
+    });
+  }
+
+  async function addProduct() {
+    if(productInformation.stateTitle !== '' &&
+       productInformation.statePrice !== null &&
+       productInformation.stateDescription !== '' &&
+       productInformation.stateCategory !== '' &&
+       productInformation.stateImage !== '' &&
+       productInformation.stateRate !== null &&
+       productInformation.stateCount !== null) {
+
+      const newProductJSON = JSON.stringify({
+        "title": productInformation.stateTitle,
+        "price": productInformation.statePrice,
+        "description": productInformation.stateDescription,
+        "category": productInformation.stateCategory,
+        "image": productInformation.stateImage,
+        "rating":{
+          "rate": productInformation.stateRate,
+          "count": productInformation.stateCount
+        }
+      })
+
+      console.log(newProductJSON);
+
+      await fetch('http://localhost:8081/add_product', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: newProductJSON
+      })
+          .then(response => response.json)
+          .then(product => {
+              console.log(product);
+          })
+
+      resetStateInformation();
+      window.location.reload();
+    }
+  }
+  
   return (
     <div>
-      <p>Hi Aren</p>
+      <div id="formInfo">
+        <div class="formInfoIndividual">
+          <label htmlFor="stateTitle">Product Title </label>
+          <input
+            id="stateTitle"
+            type="text"
+            size="40"
+            style={{marginTop: '10px', marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.stateTitle}
+            onChange={(e) => handleFieldChangeAdd('stateTitle', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="statePrice">Price of Product </label>
+          <input
+            id="statePrice"
+            type="number"
+            style={{marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.statePrice}
+            onChange={(e) => handleFieldChangeAdd('statePrice', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="stateDescription">Describe the Product </label>
+          <input
+            id="stateDescription"
+            type="text"
+            size="100"
+            style={{marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.stateDescription}
+            onChange={(e) => handleFieldChangeAdd('stateDescription', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="stateCategory">Product's Category </label>
+          <input
+            id="stateCategory"
+            type="text"
+            size="30"
+            style={{marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.stateCategory}
+            onChange={(e) => handleFieldChangeAdd('stateCategory', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="stateImage">Image (just insert the filename) </label>
+          <input
+            id="stateImage"
+            type="url"
+            size="50"
+            style={{marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.stateImage}
+            onChange={(e) => handleFieldChangeAdd('stateImage', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="stateRate">Product's Rating </label>
+          <input
+            id="stateRate"
+            type="number"
+            value={productInformation.stateRate}
+            onChange={(e) => handleFieldChangeAdd('stateRate', e.target.value)}
+            required
+          />
+        </div>
+        <div class="formInfoIndividual">
+          <label htmlFor="stateCount">Number of Ratings </label>
+          <input
+            id="stateCount"
+            type="number"
+            style={{marginLeft: '5px', fontSize: '18px'}}
+            value={productInformation.stateCount}
+            onChange={(e) => handleFieldChangeAdd('stateCount', e.target.value)}
+            required
+          />
+        </div>
+        <button id="addProduct" onClick={addProduct}>Add Product</button>
+      </div>
     </div>
+  );
+}
+
+const GET = () => {
+  fetch('http://localhost:8081/fakestore_catalog')
+    .then(response => response.json())
+    .then(fakestore_catalog => {
+      console.log(fakestore_catalog);
+
+      // Get the container that will hold all the products
+      var productsContainer = document.getElementById("getProductsContainer");
+            
+      // Populate each card
+      for(let i = 0; i < fakestore_catalog.length; i++) {
+        let individualProduct = document.createElement("div");
+        individualProduct.className = "individualProduct";
+
+          let productTitle = document.createElement("h1");
+            productTitle.className = "productTitle";
+            productTitle.innerText = fakestore_catalog[i].title;
+          individualProduct.appendChild(productTitle);
+
+          let productImage = document.createElement("img");
+            productImage.className = "productImage";
+            productImage.src = fakestore_catalog[i].image;
+          individualProduct.appendChild(productImage);
+
+          let productCategory = document.createElement("h2");
+            productCategory.className = "productCategory";
+            productCategory.innerText = `Category: ${fakestore_catalog[i].category}`;
+          individualProduct.appendChild(productCategory);
+
+          let productDescription = document.createElement("h2");
+            productDescription.className = "productDescription";
+            productDescription.innerText = fakestore_catalog[i].description;
+          individualProduct.appendChild(productDescription);
+
+          let priceAndRating = document.createElement("div");
+            let productPrice = document.createElement("h2");
+              productPrice.className = "productPrice";
+              productPrice.innerText = `Price: $${fakestore_catalog[i].price}`;
+            priceAndRating.appendChild(productPrice);
+
+            let productRating = document.createElement("h2");
+              productRating.className = "productRating";
+              productRating.innerText = `Rating: ${fakestore_catalog[i].rating.rate}/5.0 (${fakestore_catalog[i].rating.count})`
+            priceAndRating.appendChild(productRating);
+          individualProduct.appendChild(priceAndRating);
+                    
+        productsContainer.appendChild(individualProduct);
+      }
+    });
+
+  return (
+    <div>
+      <div id="getProductsContainer">
+      </div>
+    </div>
+  );
+}
+
+const PUT = () => {
+  return (
+    <div>
+      <p>PUT PAGE</p>
+    </div>
+  );
+}
+
+const DELETE = () => {
+  const [products, setProducts] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    productsForDelete();
+    if(products.length > 0) {
+      populateProduct();
+    }
+  }, []);
+
+  useEffect(() => {
+    if(products.length > 0) {
+      populateProduct();
+    }
+  }, [index]);
+
+  function hideShowButton() {
+    if(products.length > 0) {
+      let showButtonDiv = document.getElementById("showButtonDiv");
+      showButtonDiv.hidden = true;
+      populateProduct();
+    }
+  }
+
+  async function productsForDelete() {
+    await fetch('http://localhost:8081/fakestore_catalog')
+    .then(response => response.json())
+    .then(fakestore_catalog => {
+      console.log(fakestore_catalog);
+      setProducts(fakestore_catalog);
+    });
+  }
+
+  function populateProduct() {
+    let productDetails = document.getElementById("productDetails");
+    productDetails.removeAttribute("hidden");
+
+    let deleteTitle = document.getElementById("deleteTitle");
+    deleteTitle.innerText = products[index].title;
+
+    let deleteImage = document.getElementById("deleteImage");
+    deleteImage.src = products[index].image;
+
+    let deleteCategory = document.getElementById("deleteCategory");
+    deleteCategory.innerText = `Category: ${products[index].category}`;
+
+    let deleteDescription = document.getElementById("deleteDescription");
+    deleteDescription.innerText = products[index].description;
+
+    let deletePrice = document.getElementById("deletePrice");
+    deletePrice.innerText = `Price: $${products[index].price}`;
+
+    let deleteRating = document.getElementById("deleteRating");
+    deleteRating.innerText = `Rating: ${products[index].rating.rate}/5.0 (${products[index].rating.count})`
+  }
+
+function getNextForDelete() {
+  if (products.length > 0) {
+    if (index === products.length - 1) setIndex(0);
+    else setIndex(index + 1);
+  }
+}
+    
+function getPreviousForDelete() {
+  if (products.length > 0) {
+    if (index === 0) setIndex(products.length - 1);
+    else setIndex(index - 1);
+  }
+}
+
+async function deleteOneProduct() {
+  let deleteConfirmation = "You are about to delete the following product:\n\n" + products[index].title;
+
+  if(window.confirm(deleteConfirmation) == true) {
+    console.log("Product to delete: ", products[index].title);
+    
+    await fetch('http://localhost:8081/delete_product', {
+      method: "DELETE",
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({"title": products[index].title})
+    })
+      .then(response => {response.json()})
+      .then(deletedProduct => {console.log(deletedProduct)})
+      .catch((err) => console.log("Error: " + err));
+    
+    if(products.length > 1) {
+      setIndex(0);
+    }
+    window.location.reload();
+  }
+}
+
+  return (
+    <div>
+      <div id="productDetails" hidden>
+        <h1 id="deleteTitle" class="productTitle"></h1>
+        <img id="deleteImage" class="productImage"></img>
+        <h2 id="deleteCategory" class="productCategory"></h2>
+        <h2 id="deleteDescription" class="productDescription"></h2>
+        <div id="deletePriceAndRating" class="priceAndRating">
+          <h2 id="deletePrice" class="productPrice"></h2>
+          <h2 id="deleteRating" class="productRating"></h2>
+        </div>
+        <p></p>
+      </div>
+      <div id="showButtonDiv">
+        <button id="showDeleteProduct" class="showButton" onClick={hideShowButton}>SHOW PRODUCTS</button>
+      </div>
+      <div id="deleteButtons">
+        <button id="decreaseIndex" class="indexButton" onClick={getPreviousForDelete}>Prev</button>
+        <button id="deleteProduct" class="deleteButton" onClick={deleteOneProduct}>DELETE</button>
+        <button id="increaseIndex" class="indexButton" onClick={getNextForDelete}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+const ABOUT = () => {
+  return (
+    <div>
+      <p>ABOUT PAGE</p>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        {/* Navigation Bar */}
+        <nav id="navBar">
+            <button class="navButton">
+              <Link to="/post">CREATE</Link>
+            </button>
+            <button class="navButton">
+              <Link to="/get">GET ALL PRODUCTS</Link>
+              </button>
+            <button class="navButton">
+              <Link to="/put">UPDATE</Link>
+              </button>
+            <button class="navButton">
+              <Link to="/delete">DELETE</Link>
+              </button>
+            <button class="navButton">
+              <Link to="/about">ABOUT THE TEAM</Link>
+            </button>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/post" element={<POST />} />
+          <Route path="/get" element={<GET />} />
+          <Route path="/put" element={<PUT />} />
+          <Route path="/delete" element={<DELETE />} />
+          <Route path="/about" element={<ABOUT />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
